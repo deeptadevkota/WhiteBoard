abca343baf200d88d6ee824c9313f28dc1778360
deeptadevkota <deeptadevkota21@gmail.com>
2021-02-20 00:42:05 +0545

    optimised

--- canvas.js
+++ canvas.js
@@ -22,10 +22,6 @@ function publish(data) {
 
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
-
-
-
-
 //array to collect coordinates
 var plots = [];
 
@@ -42,9 +38,6 @@ function startDrawing(e) {
 
 function finishDrawing() {
     drawing = false;
-    publish({
-        plots: plots
-    });
     plots = [];
 }
 
@@ -52,19 +45,25 @@ function draw(e) {
     if (drawing == false)
         return;
     let x = e.clientX, y = e.clientY;
+    let z = null;
+    if (plots.length >= 1)
+        z = plots[plots.length - 1];
+    plots = [];
+    plots.push(z);
     plots.push({ x: x, y: y });
     drawOnCanvas(plots);
-
+    publish({
+        plots: plots
+    });
 }
+
 function drawOnCanvas(plots) {
     ctx.lineWidth = 10;
     ctx.lineCap = "round";
     ctx.beginPath();
-    ctx.moveTo(plots[0].x, plots[0].y);
-
-    for (var i = 1; i < plots.length; i++) {
-        ctx.lineTo(plots[i].x, plots[i].y);
-    }
+    if (plots.length >= 2)
+        ctx.moveTo(plots[plots.length - 2].x, plots[plots.length - 2].y);
+    ctx.lineTo(plots[plots.length - 1].x, plots[plots.length - 1].y);
     ctx.stroke();
 }
 function drawFromStream(message) {
